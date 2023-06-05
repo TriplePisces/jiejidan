8c0c20b5e7466f076ebf84dbf13b780bbf4af237
Burning <yang.zheng@funplusesport.com>
2023-06-05 10:23:23 +0800

    add

---
+++ assets/resources/index.js
@@ -0,0 +1,5 @@
+System.register("chunks:///_virtual/en.ts",["cc"],(function(e){"use strict";var n;return{setters:[function(e){n=e.cclegacy}],execute:function(){n._RF.push({},"00565nmE29LH4+tAPLoypE3","en",void 0);var t=window,a=e("languages",{staterank:"State Leaderboard"});t.languages||(t.languages={}),t.languages.en=a,n._RF.pop()}}}));
+
+System.register("chunks:///_virtual/resources",["./en.ts","./zhCN.ts"],(function(){"use strict";return{setters:[null,null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/zhCN.ts",["cc"],(function(t){"use strict";var e;return{setters:[function(t){e=t.cclegacy}],execute:function(){e._RF.push({},"fc30bfqo+9P2IAGQPEkUttE","zhCN",void 0);var a=window,n=t("languages",{staterank:"战区排行榜"});a.languages||(a.languages={}),a.languages.zhCN=n,e._RF.pop()}}}));
